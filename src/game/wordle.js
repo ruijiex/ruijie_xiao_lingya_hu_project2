7ac457bba7540f837fe1f80afb70c6a952b94e11
ruijiex <42275040+ruijiex@users.noreply.github.com>
2022-03-14 13:25:15 -0700

    Wordle: Prototype

---
+++ src/game/wordle.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+
+const WORD_LIBRARY = "./library.txt";
+
+class Wordle {
+  library = new Map();  // library: Map { k:Array }, k is word length
+  difficulties = new Set([5, 6, 7]);
+
+  difficulty = 5;
+  secret = "";
+
+  constructor(difficulty) {
+    if (difficulty !== undefined) this.difficulty = difficulty;  // difficulty: word length { easy:5, medium:6, hard:7 }
+    const words = fs.readFileSync(WORD_LIBRARY).toString().split('\n');
+
+    for (const word of words) {
+      const k = word.length;
+      if (this.difficulties.has(k)) {
+        if (!this.library.has(k))
+        this.library.set(k, []);
+
+      this.library.get(k).push(word.toUpperCase());
+      }
+    }
+
+    this.generateSecret();
+  }
+
+  generateSecret() {
+    const words = this.library.get(this.difficulty);
+    this.secret = words[Math.floor(Math.random() * words.length)];
+  }
+
+  guess(word) {
+    if (word.length != this.difficulty) return [];
+
+    const maps = new Map();  // secret frequency map
+    for (const char of this.secret) {
+      if (!maps.has(char))
+        maps.set(char, 0);
+      maps.set(char, maps.get(char) + 1);
+    }
+
+    // color check has priority: need two trivial loops - check 2 before 1, case: secret = "aba", guess = "aaa"
+    const colors = Array(this.difficulty).fill(0);  // 2: char and pos match, 1: char exists but pos doesn't
+
+    for (let i = 0; i < this.difficulty; i++) {
+      const char = word[i].toUpperCase();
+
+      if (char === this.secret[i]) {
+        if (maps.get(char) > 0) {
+          colors[i] = 2;  // found a match: push green
+
+          maps.set(char, maps.get(char) - 1);
+        }
+      }
+    }
+
+    for (let i = 0; i < this.difficulty; i++) {
+      const char = word[i].toUpperCase();
+
+      if (char !== this.secret[i]) {
+        if (maps.get(char) > 0) {
+          colors[i] = 1;  // the char exists in the secret, but pos doesn't match: scan left to right
+
+          maps.set(char, maps.get(char) - 1);
+        }
+      }
+    }
+
+    return colors;
+  }
+}
+
+module.exports = Wordle;
