9fe2296a38cefe1cf7315dba841135b84d7f751d
lingya <hly19971203@hotmail.com>
2022-03-18 14:53:59 -0700

    Add guessed word saving function to Game page

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import NavigationBar from './NavigationBar';
 import { useDispatch, useSelector } from 'react-redux';
 import changeDifficultyAction from '../actions/changeDifficultyAction';
+import { useState } from 'react';
 
 export default function Game() {
   const dispatch = useDispatch();
@@ -11,13 +12,41 @@ export default function Game() {
   if (stateDifficulty !== difficulty) {
     dispatch(changeDifficultyAction(difficulty));
   }
+  const [inputWord, setInputWord] = useState('');
+  const [guessedWords, setGuessedWords] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setGuessedWords((oldWords) => [...oldWords, inputWord]);
+    setInputWord('');
+  };
 
+  // TODO
+  const secret = '';
   return (
     <div>
       <NavigationBar />
       <h1>Game</h1>
-      <h1>{difficulty}</h1>
       <p>current state difficulty: {stateDifficulty}</p>
+      <p>Current secret: {secret}</p>
+      <form onSubmit={handleSubmit}>
+        {/* TODO 
+        Set number of input from difficulty
+        */}
+        <input
+          type='text'
+          required
+          value={inputWord}
+          onChange={(e) => setInputWord(e.target.value)}
+        ></input>
+        <button type='submit'>Click to submit</button>
+      </form>
+      <ul>
+        {guessedWords.map((value, index) => {
+          // TODO
+          // return letter state
+          return <li key={index}>{value}</li>;
+        })}
+      </ul>
     </div>
   );
 }
