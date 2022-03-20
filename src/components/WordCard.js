5502438583c9359384f0df4f5c8cd1d2433488f3
ruijiex <42275040+ruijiex@users.noreply.github.com>
2022-03-20 13:31:40 -0700

    WordCard: Adding Hints Styling

--- src/components/WordCard.js
+++ src/components/WordCard.js
@@ -2,9 +2,18 @@ import React from 'react';
 import '../css/WordCard.css';
 
 export default function WordCard(props) {
+  const chars = [];
+  for (const char of props.guessedWord)
+    chars.push(char);
+
   return (
-    <li class="guess-attempt">
-      {props.guessedWord} {props.guessState}
+    <li className="guess-attempt">
+      {chars.map((char, index) => {
+        return (
+          <span class={"color-hint-" + props.guessState[index].toString()}>{char}</span>
+        );
+      })}
+
     </li>
   );
 }
