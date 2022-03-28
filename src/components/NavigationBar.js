3442ca553a48ce84c9f3bf899cb49264fb92c5e5
ruijiex <42275040+ruijiex@users.noreply.github.com>
2022-03-27 22:05:25 -0700

    Navigation Bar Refactor

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import '../css/NavigationBar.css';
 
 export default function NavigationBar() {
   const stateDifficulty = useSelector((state) => state.changeDifficulty);
+  const [active, setActive] = useState('home');
 
   return (
     <Navbar bg='light' expand='lg'>
@@ -12,14 +14,39 @@ export default function NavigationBar() {
         <Navbar.Brand href='/'>Wordle</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto' defaultActiveKey='/'>
-            <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link eventKey='game' href={'/game/' + stateDifficulty}>
-              Game
-            </Nav.Link>
-            <Nav.Link eventKey='rules' href='/rules'>
-              Rules
-            </Nav.Link>
+          <Nav
+            variant='tabs'
+            className='me-auto'
+            activeKey={active}
+            onSelect={(chosen) => setActive(chosen)}
+          >
+            <Nav.Item>
+              <Nav.Link
+                eventKey='home'
+                href='/'
+                onSelect={(chosen) => setActive(chosen)}
+              >
+                Home
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='game'
+                href={'/game/' + stateDifficulty}
+                onSelect={(chosen) => setActive(chosen)}
+              >
+                Game
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='rules'
+                href='/rules'
+                onSelect={(chosen) => setActive(chosen)}
+              >
+                Rules
+              </Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
