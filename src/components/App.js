d00f19e33f7141c7fe04464d2da4053c9c7e78fa
lingya <hly19971203@hotmail.com>
2022-03-28 18:28:28 -0700

    Fix navigation routing active issue

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import Home from './Home';
 import Game from './Game';
 import Rules from './Rules';
 import { Route, Routes } from 'react-router-dom';
+import NavigationBar from './NavigationBar';
 
 export default function App() {
   return (
     <div className='App'>
+      <NavigationBar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='game/:difficulty' element={<Game />} />
-        <Route path='rules' element={<Rules />} />
+        <Route exact path='/' element={<Home />} />
+        <Route path='/game/:difficulty' element={<Game />} />
+        <Route path='/rules' element={<Rules />} />
       </Routes>
     </div>
   );
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,55 +1,28 @@
 import React from 'react';
-import { useState } from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import '../css/NavigationBar.css';
-
+import { LinkContainer } from 'react-router-bootstrap';
 export default function NavigationBar() {
   const stateDifficulty = useSelector((state) => state.changeDifficulty);
-  const [active, setActive] = useState('home');
 
   return (
     <Navbar bg='light' expand='lg'>
-      <Container>
-        <Navbar.Brand href='/'>Wordle</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav
-            variant='tabs'
-            className='me-auto'
-            activeKey={active}
-            onSelect={(chosen) => setActive(chosen)}
-          >
-            <Nav.Item>
-              <Nav.Link
-                eventKey='home'
-                href='/'
-                onSelect={(chosen) => setActive(chosen)}
-              >
-                Home
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                eventKey='game'
-                href={'/game/' + stateDifficulty}
-                onSelect={(chosen) => setActive(chosen)}
-              >
-                Game
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                eventKey='rules'
-                href='/rules'
-                onSelect={(chosen) => setActive(chosen)}
-              >
-                Rules
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand>Wordle</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <LinkContainer to='/'>
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to={'/game/' + stateDifficulty}>
+            <Nav.Link>Game</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/rules'>
+            <Nav.Link>Rules</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
